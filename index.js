5cda4a1d1a35bb02b18b9b75eb54c99c667d4b6a
david986912425 <66333142+david986912425@users.noreply.github.com>
2022-11-07 04:34:19 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -7,18 +7,16 @@ var btnsData = document.querySelector('.btns-data');
 var dataContainer = document.querySelector('.data-container');
 var dowloaderContainer = document.querySelector('.dowloader-container');
 var TABLA = document.querySelector('#table')
-
+var ruc = [];
 
 button.addEventListener('click', () => {
-    console.log('TABLA clicked');
+    
     if (ArchivoXML) {
         CargarXMLNEW();
         inputContainer.style.display = "none"
         btnsData.style.display = "flex";
         dataContainer.style.display = "flex";
         dowloaderContainer.style.display = "flex";
-        
-        console.log('abre');
 
     } else {
         console.log('no abre');
@@ -72,44 +70,114 @@ const CargarXML = (xml) => {
     let DocumentoXML = xml;
 
     let currentDate = new Date();
-
+    let importe = parseFloat(DocumentoXML.getElementsByTagName("cbc:Amount")[0].childNodes[0].nodeValue).toFixed(2);
     let nropedido = DocumentoXML.getElementsByTagName("cbc:ID")[2].childNodes[0].nodeValue;
     let fechaFactura = DocumentoXML.getElementsByTagName("cbc:IssueDate")[0].childNodes[0].nodeValue;
     let refer = DocumentoXML.getElementsByTagName("cbc:ID")[0].childNodes[0].nodeValue;
     let fechaContab = `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`;
     let texto = DocumentoXML.getElementsByTagName("cbc:Description")[0].childNodes[0].nodeValue;
-    let valorVenta = parseFloat(DocumentoXML.getElementsByTagName("cbc:TaxableAmount")[0].childNodes[0].nodeValue).toFixed(2);
-    var IGV = parseFloat(valorVenta*0.18).toFixed(2); //Aca se calcula el IGV
-    let importeTotal = parseFloat(valorVenta)+parseFloat(IGV);  //Aca se calcula el Importe Total
-    var detraccion = parseFloat(importeTotal*0.12).toFixed(2); //Aca se calcula el detraccion
-    let importe = parseFloat(DocumentoXML.getElementsByTagName("cbc:Amount")[0].childNodes[0].nodeValue).toFixed(2); //Aca se calcula el importe
-    console.log('detraccion:'+ (detraccion+IGV));
+    let valorVenta = parseFloat(DocumentoXML.getElementsByTagName("cbc:TaxableAmount")[0].childNodes[0].nodeValue).toFixed(2); //Aca se calcula el importe
+    var IGV = parseFloat(DocumentoXML.getElementsByTagName("cbc:TaxAmount")[0].childNodes[0].nodeValue).toFixed(2);
+    let importeTotal = parseFloat(DocumentoXML.getElementsByTagName("cbc:PayableAmount")[0].childNodes[0].nodeValue).toFixed(2);
+    var detraccion = 0;
+    var retencion = 0;
+    var importeConRetencion = 0;
+    var boolRet = "NO";
+    var motivoRet = "SE ENCUENTRA EN EL PADRÓN DE RETENCIÓN";
+    
+    let rucid = DocumentoXML.getElementsByTagName("cac:PartyIdentification")[0].children[0].innerHTML;
+    console.log(rucid);
+    
+    if (IGV > 0) {
 
+        detraccion = parseFloat(importeTotal*0.12).toFixed(2);
+        
+        if (importeTotal > 700) {
+            fetch('RUCELECTRONICOS.json')
+            .then(data => data.json())
+            .then(data=> {
+                ruc = data;
+                
+                const resultado = ruc.filter(function(data){
+                    
+                    if ( data.Ruc == rucid){
+                        return true;
+                    }
+                    
+                });
 
+                if(resultado.length > 0){
+                    console.log('Si se encuentra en el Padron de retencion,entonces no se debe cobrar retencion');
+                }else{
+                    console.log('No se encuentra en el Padron de retencion,entonces si se debe cobrar retencion');
+
+                    boolRet = "SI";
+                    retencion = importeTotal * 0.03;
+                    importeConRetencion = importeTotal + retencion;
+                    motivoRet = "NO SE ENCUENTRA EN EL PADRÓN DE RETENCIÓN";
+                }
+            });
+        }
+    } else{
+        console.log('No se le aplica retencion ni detracción');
+        motivoRet = "SIN IGV";
+    }
+    
+    
     
 
-
-
-    let arrayData = [nropedido, fechaFactura, refer, fechaContab, valorVenta, IGV,importeTotal, detraccion, importe,texto];
+    let arrayData = [nropedido, fechaFactura, refer, fechaContab, valorVenta, IGV,importeTotal, detraccion, importe, texto, boolRet, motivoRet, importeConRetencion, retencion];
 
     let dataHTML = document.querySelector('.data-right');
+    let dataHTML2 = document.querySelector('.data-left');
     let dataTable = document.querySelector('#data');
+    let inputMotivoRet = document.querySelector('#motRetencion');
+    let inputMotivoRet2 = document.querySelector('#motRetencion2');
+    let inputImporteRetencion = document.querySelector('#importeRetencion');
+    let inputImporteRetencion2 = document.querySelector('#importeRetencion2');
+    let inputPorcentajeRetencion = document.querySelector('#PorcentajeRetencion');
+    let inputPorcentajeRetencion2 = document.querySelector('PporcentajeRetencion2');
+
     dataHTML.innerHTML = "";
     dataTable.innerHTML = "";
-    for (let i = 0; i < arrayData.length; i++) {
-        dataHTML.innerHTML += `
-            <div class="value">
-                ${arrayData[i]}
-            </div>
-        `
-        dataTable.innerHTML += `
-            <td>
-                ${arrayData[i]}
-            </td>
-        `
-    }
 
-    
+    if (arrayData[10] == "NO") {
+        for (let i = 0; i < arrayData.length - 2; i++) {
+            dataHTML.innerHTML += `
+                <div class="value">
+                    ${arrayData[i]}
+                </div>
+            `
+            dataTable.innerHTML += `
+                <td>
+                    ${arrayData[i]}
+                </td>
+            `
+        }
+
+        inputImporteRetencion.remove();
+        inputImporteRetencion2.remove();
+        inputPorcentajeRetencion.remove();
+        inputPorcentajeRetencion2.remove();
+    } else {
+        for (let i = 0; i < arrayData.length; i++) {
+            if (i != 10) {
+                dataHTML.innerHTML += `
+                <div class="value">
+                    ${arrayData[i]}
+                </div>
+            `
+            dataTable.innerHTML += `
+                <td>
+                    ${arrayData[i]}
+                </td>
+            `
+            }
+        }
+
+        inputMotivoRet.remove();
+        inputMotivoRet2.remove();
+    }
 }
 
 
